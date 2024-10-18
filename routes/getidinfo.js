d2a36665e3b93d455b241dc3347f1739d6260270
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2024-10-18 17:28:32 +0530

    Update getidinfo.js

--- routes/getidinfo.js
+++ routes/getidinfo.js
@@ -3,19 +3,51 @@ const axios = require("axios");
 
 const route = express.Router();
 
-
 route.get("/getidinfo", async (req, res) => {
-    let malId = req.query.malId;
-    try {
-        const response = await axios(`https://miyou-api-steal.vercel.app/?etc=api%2Fgetidinfo%3FmalId%3D${malId}`);
-        const data = response.data;
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(404).json({
-            error: error.message
-        });
+  let malId = req.query.malId;
+  try {
+    const resJson = {};
+    const response = await axios(
+      `https://api.malsync.moe/mal/anime/anilist:${malId}`
+    );
+    const gogoData = response.data.Sites?.Gogoanime;
+    if (!gogoData) {
+      return res.status(404).json({ error: "No data found" });
     }
+    const keys = Object.keys(gogoData);
+    const isDub = keys.length > 1;
+    resJson["isDub"] = isDub;
+
+    let subLink, dubLink;
+
+    if (isDub) {
+      subLink = gogoData[keys.find((key) => !key.includes("dub"))].identifier;
+      resJson["subLink"] = subLink;
+      dubLink = gogoData[keys.find((key) => key.includes("dub"))].identifier;
+      resJson["dubLink"] = dubLink;
+      const subRes = await axios(
+        `https://tofu-consumet.vercel.app/anime/gogoanime/info/${subLink}`
+      );
+      resJson["subTotalEpisodes"] = subRes.data.totalEpisodes;
+      const dubRes = await axios(
+        `https://tofu-consumet.vercel.app/anime/gogoanime/info/${dubLink}`
+      );
+      resJson["dubTotalEpisodes"] = dubRes.data.totalEpisodes;
+    } else {
+      subLink = gogoData[keys[0]].identifier;
+      const subRes = await axios(
+        `https://tofu-consumet.vercel.app/anime/gogoanime/info/${subLink}`
+      );
+      resJson["subTotalEpisodes"] = subRes.data.totalEpisodes;
+    }
+
+    return res.status(200).json(resJson);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({
+      error: error.message,
+    });
+  }
 });
 
 module.exports = route;
